6ba8bcf77868651f39b184579fbb70522a18da2b
Robin <Xergxes7@users.noreply.github.com>
2024-10-26 19:16:37 +0200

    Enhancements
    - spawn prop if configured
    - use name metadata
    - spawn props at targets

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,6 @@
 RegisterNetEvent('qb-printer:client:UseDocument', function(ItemData)
     local DocumentUrl = ItemData.info.url ~= nil and ItemData.info.url or false
+    exports['rpemotes']:EmoteCommandStart('clipboard',nil)
     SendNUIMessage({
         action = "open",
         url = DocumentUrl
@@ -24,17 +25,74 @@ RegisterNetEvent('qb-printer:client:SpawnPrinter', function()
     SetEntityAsMissionEntity(obj)
 end)
 
+
+
+CreateThread(function()
+    for k,v in pairs(Config.PrinterLocations) do
+        if v.spawn then
+        SpawnPresetPrinter(v.location.x,v.location.y,v.location.z,v.heading,v.size)
+     
+        --print("Printer Spawned: " .. k)
+        end
+        exports['qb-target']:AddBoxZone(k, v.location, 1.0, 1.5, {
+            name = k,
+            heading = v.heading,
+            minZ = v.location.z - 1,
+            maxZ = v.location.z + 1,
+            debugPoly = false,
+        }, {
+            options = {
+                {
+                    type = 'client',
+                    event = 'qb-printer:printer',
+                    icon = "fa fa-print	",
+                    label = Lang:t('info.use_printer'),
+                }
+            },
+            distance = 1.5
+        })
+        --print("Printer Target: " .. k)
+    end
+end)
+
+
+function SpawnPresetPrinter(x,y,z,heading,size)
+    local model = `prop_printer_01`
+
+    if size == 1 then 
+        model =  `v_res_printer`
+    elseif size == 2 then 
+        model = `prop_printer_02`
+    elseif size == 3 then
+        model = `proper_printer_01`
+    elseif size == 4 then
+        model = `prop_copier_01`
+    end
+
+    RequestModel(model)
+    while (not HasModelLoaded(model)) do
+        Wait(1)
+    end
+    local obj = CreateObject(model, x, y, z, true, false, true)
+    PlaceObjectOnGroundProperly(obj)
+    SetEntityHeading(obj, heading)
+    FreezeEntityPosition(obj, true)
+    SetModelAsNoLongerNeeded(model)
+    --SetModelAsNoLongerNeeded(model)
+    --SetEntityAsMissionEntity(obj)
+end
 -- NUI
 
 RegisterNUICallback('SaveDocument', function(data, cb)
     if data.url then
-        TriggerServerEvent('qb-printer:server:SaveDocument', data.url)
+        TriggerServerEvent('qb-printer:server:SaveDocument', data.url, data.name)
     end
     cb('ok')
 end)
 
 RegisterNUICallback('CloseDocument', function(_, cb)
     SetNuiFocus(false, false)
+    exports['rpemotes']:EmoteCancel()
     cb('ok')
 end)
 
@@ -59,7 +117,7 @@ RegisterNetEvent('qb-printer:printer',function()
 end)
 
 if Config.UseTarget then
-    CreateThread(function()
+    --[[CreateThread(function()
         exports['qb-target']:AddTargetModel("prop_printer_01", {
             options = {
                 {
@@ -71,5 +129,5 @@ if Config.UseTarget then
             },
             distance = 1.5,
         })
-    end)
+    end)]]
 end
